5d5a8e9acf934d0b38ef0db4e1e69f530c54d68f
Dylan Lloyd <dylan@dylansserver.com>
2012-06-25 15:39:15 -0400

    reformatted & reflowed to hand off to the callback

--- share/spice/campaign_finance/spice.js
+++ share/spice/campaign_finance/spice.js
@@ -1,27 +1,70 @@
+var items = new Array();
+items[0] = new Array();
+items[0]['s'] = 'Sunlight Foundation';
+items[0]['h'] = 'Campaign finance summary';
+items[0]['u'] = 'http://influenceexplorer.com/';
 
-function ddg_spice_campaign_finance(candidate) {
-    var snippet = '';
+var industries = {
+    'A' : 'Agribusiness',
+    'B' : 'Communications/Electronics',
+    'C' : 'Construction',
+    'D' : 'Defense',
+    'E' : 'Energy/Natural Resources',
+    'F' : 'Finance/Insurance/Real Estate',
+    'H' : 'Health',
+    'K' : 'Lowyers and Lobbyists',
+    'M' : 'Transportation',
+    'N' : 'Misc. Business',
+    'Q' : 'Ideology/Single Issue',
+    'P' : 'Labor',
+    'W' : 'Other',
+    'Y' : 'Unknown',
+    'Z' : 'Adminstrative Use'
+};
 
-    if (candidate) {
-       if (candidate.length > 1) {
-    	   snippet = format_list(candidate);
-       }
-       else {
-           snippet = format_snippet(candidate);    	   
-       }
-       items = new Array();
-       items[0] = new Array();
-       items[0]['a'] = snippet;
-       items[0]['h'] = 'Campaign finance summary';
-       items[0]['s'] = 'Sunlight Foundation';
-       items[0]['u'] = 'http://influenceexplorer.com/';
-       console.log(items);
-       nra(items);
+function ddg_spice_campaign_finance(candidates) {
+    console.log(candidates);
+    if (candidates) {
+        for (i=0;i<candidates.length;i++){
+            if (candidates[i].seat == null) console.log(candidates.splice(i,1));
+        }
+        if (candidates.length > 1) {
+            items[0]['a'] = '';
+            for (i=0;i<candidates.length;i++){
+                items[0]['a'] += '<a href = "/?q=sunlight+'
+                              +  encodeURI(candidates[i]['name'])
+                              +  '">' + candidates[i]['name']
+                              +  '</a>\t' + candidates[i]['seat'] + '<br>';
+            }
+            nra(items);
+        } else {
+             items[0]['a'] = candidates[0]['name']
+                           + ' has received a total of $'
+                           + addCommas(candidates[0]['total_received'])
+                           + ' campaigning for the office of '
+                           + candidates[0]['seat'].split(':')[1] + '.<br>';
+             var callback = "http://transparencydata.com/api/1.0/aggregates/pol/"
+                          + candidates[0].id
+                          + "/contributors/sectors.json?apikey="
+                          + "81ae602f16f34cbc9fe2643c7691f3d3"
+                          + "&callback=ddg_spice_campaign_finance_industry";
+             nrj(callback);
+        }
     }
 }
 
-function addCommas(nStr)
-{
+
+function ddg_spice_campaign_finance_industry(response) {
+    items[0]['a'] += 'Top industries:<br>';
+	for (i=0;i<response.length;i++) {
+		items[0]['a'] += industries[response[i]['sector']] + ": $"
+                      + addCommas(response[i]['amount']) + '<br>';
+	}
+    nra(items);
+}
+
+
+function addCommas(nStr) {
 	nStr += '';
 	x = nStr.split('.');
 	x1 = x[0];
@@ -33,42 +76,3 @@ function addCommas(nStr)
 	return x1 + x2;
 }
 
-
-
-function format_list(candidates) {
-	var list, div;
-	
-	list = d.createElement('span');
-	div = d.createElement('div');
-	div.innerHTML = ''
-	for (i=0;i<candidates.length;i++){
-		div.innerHTML += '<a href = "http://127.0.0.1:5000/?q=sunlight+' + encodeURI(candidates[i]['name']) + '">' + candidates[i]['name'] + '</a>\t' + candidates[i]['seat'] + '<br>';
-	}
-	list.appendChild(div);
-	return list;
-}	
-
-
-
-function format_snippet(candidate){
-	var snippet, div;
-	
-	snippet = d.createElement('span');
-    div = d.createElement('div');
-    div.innerHTML = candidate[0]['name'] + ' has received a total of $' + addCommas(candidate[0]['total_received']) + ' campaigning for the office of ' + candidate[0]['seat'].split(':')[1] + '.';    
-    div.innerHTML += '<br>Top industries: <br>' + nrj("http://transparencydata.com/api/1.0/aggregates/pol/f99f7d244bbf4325b31312f75643f8da/contributors/sectors.json?apikey=81ae602f16f34cbc9fe2643c7691f3d3&callback=ddg_spice_campaign_finance_industry");
-    
-    snippet.appendChild(div);
-
-    return snippet;
-}
-
-function ddg_spice_campaign_finance_industry(response) {
-	var industry_list = "";
-	for (i=0;i<response.length;i++) {
-		industry_list += response[i]['sector'] + ": $" + addCommas(response[i]['amount']) + '<br>';
-	};
-	console.log("list from _industry: " +industry_list);
-	return industry_list;
-	
-}
