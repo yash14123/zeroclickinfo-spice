9be2f0aa267f8b145e4f406bded70b807ff372ae
Dylan Lloyd <dylan@dylansserver.com>
2012-08-01 21:56:59 -0400

    added missed files

---
+++ lib/DDG/Spice/Exoplanets/Discovery-e.pm
@@ -0,0 +1,15 @@
+package DDG::Spice::Exoplanets::Discovery-e;
+
+use DDG::Spice;
+
+triggers any => "exoplanet", "exoplanets", "planet", "planets";
+spice to => 'http://exoapi.com/api/planets/search?discoveryyear:gt=$1&limit=5&jsonp={{callback}}';
+
+handle query_lc => sub {
+    if (/discovered after (\d{2,4})/) {
+        return "$1";
+    }
+    return '0';
+};
+
+1;
