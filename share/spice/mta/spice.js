727d295e882f4c2632533d1139870f8fb53db8cf
Dylan Lloyd <dylan@dylansserver.com>
2013-01-15 23:20:55 -0500

    put results into a list format

--- share/spice/mta/spice.js
+++ share/spice/mta/spice.js
@@ -5,7 +5,7 @@ function ddg_spice_mta(response) {
 
     var busses = response.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit;
 
-    var answer  = '';
+    var answer  = "<ul>";
 
     var query = busses[0].MonitoredVehicleJourney.MonitoredCall.StopPointName;
 
@@ -14,17 +14,18 @@ function ddg_spice_mta(response) {
         var busline = bus.LineRef.replace(/^MTA NYCT_/, "");
         var distance = bus.MonitoredCall.Extensions.Distances.PresentableDistance;
         var direction = bus.DirectionRef;
-        answer += "There is a" + (direction == 1 ? "n uptown " : " downtown ")
-               +  busline + " bus " + distance + ".<br>";
+        answer += "<li>There is a" + (direction == 1 ? "n uptown " : " downtown ")
+               +  busline + " bus " + distance + ".</li>";
     }
 
+    answer += "</ul>";
 
 	var items = new Array();
 	items[0] = new Array();
     items[0]['a'] = answer;
 	items[0]['h'] = query + " (MTA)";
-	items[0]['s'] = 'MTA';
-	items[0]['u'] = 'http://www.bustime.mta.info/';
+	items[0]['s'] = "the MTA";
+	items[0]['u'] = "http://www.bustime.mta.info/";
     items[0]["force_big_header"] = true;
 	
 	nra(items);
