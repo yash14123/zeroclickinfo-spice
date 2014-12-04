b12e17fcac250d24e5e3b27658b0c814e746f5e9
jagtalon <jag@jagtalon.com>
2014-12-04 17:56:22 +0000

    Kngine: Add stricter regexp.

--- lib/DDG/Spice/Kngine.pm
+++ lib/DDG/Spice/Kngine.pm
@@ -18,21 +18,11 @@ triggers any => "height", "tall";
 spice to => 'http://api.kngine.com/SearchAPI1.ashx?key=CFF5EE1C4AD94B5DA27DFFE6AA5B8472&q=$1';
 spice wrap_jsonp_callback => 1;
 
-my @guard = (
-    qr/^(?:what(?: is| was|'?s) the |)(?:(?:number|height|volume|diameter|length|size|area|circumference|capital|speed) of|mass|radius|density|distance|elevation|population|age|(?:birth|death)\s?(?:year|date|day)|weight|sun(?:rise|set)|tides?)\s+.*$/,
-    qr/^.*\s+(?:mass|radius|diameter|prime|surjective|density|distance|elevation|height|population|age|(?:birth|death)\s?(?:year|date|day)|weight|sun(?:rise|set)|tides?)$/,
-    qr/\bhow many\b.*\b(?:in|on)\b/,
-    qr/^[^\s]+ in a (?:year|month|mile|day|hour|gallon|quart|pint|meter|ton|km|kilometer|pound|kilo|kilogram)$/,
-);
-
 handle query_lc => sub {
     return unless $_;
 
-    foreach my $g (@guard) {
-        warn $g;
-        if($_ =~ $g) {
-            return $_;
-        }
+    if($_ =~ qr/^(?:what(?: is| was|'?s) the |)(?:(?:number|height|volume|diameter|length|size|area|circumference|capital|speed) of|mass|radius|density|distance|elevation|population|age|(?:birth|death)\s?(?:year|date|day)|weight|sun(?:rise|set)|tides?)\s+.*$/ || ($_ =~ qr/^.*\s+(?:mass|radius|diameter|prime|surjective|density|distance|elevation|height|population|age|(?:birth|death)\s?(?:year|date|day)|weight|sun(?:rise|set)|tides?)$/ && $_ !~ qr/^(?:what\'s |what is | what are |)my/) || $_ =~ qr/\bhow many\b.*\b(?:in|on)\b/ || $_ =~ qr/^[^\s]+ in a (?:year|month|mile|day|hour|gallon|quart|pint|meter|ton|km|kilometer|pound|kilo|kilogram)$/) {
+        return $_;
     }
 
     return;
